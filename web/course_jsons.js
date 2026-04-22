e440b25beab517df64f410a904582274b5e29ead
Achintya <34377519+achintya-labs@users.noreply.github.com>
2026-04-23 02:16:10 +0530

    Changed sem name and added new dark/light toggle and footer

--- web/course_jsons.js
+++ web/course_jsons.js
@@ -7,6 +7,6 @@ Only fetch when selected.
 */
 
 const SEMESTER_DATA_FILES = {
-    "Aug-Nov 2026": "./data/AUG-NOV-2026.json",
+    "Jul-Nov 2026": "./data/JUL-NOV-2026.json",
     "Jan-May 2026": "./data/JAN-MAY-2026.json"
 };
